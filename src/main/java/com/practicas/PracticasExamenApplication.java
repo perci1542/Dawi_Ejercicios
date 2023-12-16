a7c35b98da6d277b4bec09e73027ec6b20f6ead8
Perzival1542 <razemaster426@gmail.com>
2023-12-16 08:16:34 -0500

    Pruebas para examen

---
+++ src/main/java/com/practicas/PracticasExamenApplication.java
@@ -0,0 +1,13 @@
+package com.practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicas/PracticasExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
