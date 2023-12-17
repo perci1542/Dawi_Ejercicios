865c044969c6e70236f409049eede1d8514af090
Perzival1542 <razemaster426@gmail.com>
2023-12-17 11:31:30 -0500

    Tercera parte con Validacion

--- src/main/java/com/practicas/controller/Practica_Controller.java
+++ src/main/java/com/practicas/controller/Practica_Controller.java
@@ -1,6 +1,7 @@
 package com.practicas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +18,20 @@ public class Practica_Controller {
 	
 	@PostMapping("/login")
 	public String ValidarAcceso(@RequestParam("txt_Usuario") String usuario,
-			                    @RequestParam("txt_Contraseña") String contraseña) 
-	{
+			                    @RequestParam("txt_Contraseña") String contraseña,
+			                    Model model) 
+	{		
 		
-		System.out.println(usuario + " " + contraseña);
+		
+		if(usuario.equals("razemaster426@gmail.com") && contraseña.equals("raze1542")) {
+			model.addAttribute("Mensaje","Bienvenido : " + usuario );
+			model.addAttribute("cssMensaje","alert alert-success");
+		}
+		
+		else {
+			model.addAttribute("Mensaje","Usuario o Clave Incorrecto" );
+			model.addAttribute("cssMensaje","alert alert-danger");
+		}
 		
 		return "login";
 	}
